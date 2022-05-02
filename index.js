573bddde231a12d27e312515b06567e59a7551da
Faez-B <faezbhatti@gmail.com>
2022-05-02 16:00:19 +0200

    OK : Question 4 résolu, si on entre une route qui n'est pas défini alors on affiche un message disant 'Page introuvable'

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const http = require('http');
 const server = http.createServer( (req , res) => {
     // console.log(req);
     if (req.url === '/') {
+        
+        res.writeHead(200, { 'content-type': 'text/html' });
+
         if (req.method === 'GET') {
             res.write("<h1>HELLO WORLD FAEZ</h1>");   
         }  
@@ -12,8 +15,14 @@ const server = http.createServer( (req , res) => {
             res.write("<h1>400 Méthode non autorisée</h1>");   
         }
 
-        res.end();
     }
+    
+    else{
+        res.writeHead(404, { 'content-type': 'text/html' });    
+        res.write("<h1>404 Page introuvable</h1>");
+    }
+
+    res.end();
 });
 
 server.listen(8000);
