d3c6b3ca244d579d2a9a96d00be61e84518e51fd
Faez-B <faezbhatti@gmail.com>
2022-05-02 16:09:20 +0200

    OK : Question 5 résolu, lorsqu'on rencontre une erreur interne au serveur on renvoie le message correspondant

--- index.js
+++ index.js
@@ -2,24 +2,32 @@ const http = require('http');
 
 const server = http.createServer( (req , res) => {
     // console.log(req);
-    if (req.url === '/') {
-        
-        res.writeHead(200, { 'content-type': 'text/html' });
+    try {
+        // console.loga(); // Pour provoquer une erreur et renvoyer l'erreur 500
 
-        if (req.method === 'GET') {
-            res.write("<h1>HELLO WORLD FAEZ</h1>");   
-        }  
-
-        else {
-            // exemple : curl -X PUT localhost:8000
-            res.write("<h1>400 Méthode non autorisée</h1>");   
-        }
-
-    }
+        if (req.url === '/') {
+            
+            res.writeHead(200, { 'content-type': 'text/html' });
     
-    else{
-        res.writeHead(404, { 'content-type': 'text/html' });    
-        res.write("<h1>404 Page introuvable</h1>");
+            if (req.method === 'GET') {
+                res.write("<h1>HELLO WORLD FAEZ</h1>");   
+            }  
+    
+            else {
+                // exemple : curl -X PUT localhost:8000
+                res.write("<h1>400 Méthode non autorisée</h1>");   
+            }
+    
+        }
+        
+        else{
+            res.writeHead(404, { 'content-type': 'text/html' });    
+            res.write("<h1>404 Page introuvable</h1>");
+        }
+        
+    } catch (error) {
+        res.writeHead(500, { 'content-type': 'text/html' });    
+        res.write("<h1>500 Erreur Interne au Serveur</h1>");
     }
 
     res.end();
