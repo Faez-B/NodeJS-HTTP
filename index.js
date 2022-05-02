fe848a8bf0faae1411ffb310ef20edf83fc63302
Faez-B <faezbhatti@gmail.com>
2022-05-02 15:56:59 +0200

    OK : Question 3 résolu, si on utilise une autre méthode que GET alors on a un message différent

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const http = require('http');
 
 const server = http.createServer( (req , res) => {
     // console.log(req);
-    if (req.url === '/' && req.method === 'GET') {  
-        res.write("<h1>HELLO WORLD FAEZ</h1>");   
+    if (req.url === '/') {
+        if (req.method === 'GET') {
+            res.write("<h1>HELLO WORLD FAEZ</h1>");   
+        }  
+
+        else {
+            // exemple : curl -X PUT localhost:8000
+            res.write("<h1>400 Méthode non autorisée</h1>");   
+        }
+
         res.end();
     }
 });
