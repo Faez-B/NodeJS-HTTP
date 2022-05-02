21a159559a0fb6d9f368b59db8da271d3970286c
Faez-B <faezbhatti@gmail.com>
2022-05-02 15:49:35 +0200

    OK : Question 2 résolu. Changement de port.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const http = require('http');
 
 const server = http.createServer( (req , res) => {
-    console.log("HELLO WORLD FAEZ");
-
+    // console.log(req);
+    if (req.url === '/' && req.method === 'GET') {  
+        res.write("<h1>HELLO WORLD FAEZ</h1>");   
+        res.end();
+    }
 });
 
-server.listen(4000);
+server.listen(8000);
